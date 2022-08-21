2cbe946a7482444f176ec81b1a38fa3f9b8f89b0
Shundrik <102665832+Shundrik@users.noreply.github.com>
2022-08-21 21:26:38 +0300

    hw-6
    finished 9 task

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,31 @@
+const form = document.querySelector(".login-form");
 
+form.addEventListener("submit", onFormSubmit);
+
+function onFormSubmit(event) {
+  event.preventDefault();
+
+  const emailEl = event.currentTarget.email.value;
+  const passwordEl = event.currentTarget.password.value;
+
+  if (emailEl === "" || passwordEl === "") {
+    alert("applay the form");
+        return;
+  }
+  const formData = new FormData(event.currentTarget);
+
+  formData.forEach((value, name) => {
+    // console.log(formData);
+    // console.log("name:", name);
+    // console.log("value:", value);
+
+    const dataForm = `${name}: ${value}`;
+    console.log(dataForm);
+
+    form.reset()
+
+    return dataForm;
+  });
+  
+}
+  
--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,20 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const spanRef = document.querySelector(".color");
+const buttonRef = document.querySelector(".change-color");
+
+buttonRef.addEventListener("click",onBodyChange)
+
+console.log(spanRef.textContent);
+console.log(buttonRef);
+
+
+function onBodyChange(event){
+  console.log(event);
+  const spanTextRef = spanRef.textContent = getRandomHexColor();
+  console.log(spanRef.textContent);
+  document.body.style.backgroundColor = spanTextRef ;
+}
+
