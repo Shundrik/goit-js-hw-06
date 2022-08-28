081fcadee8db47670e0eac87d7eab2982aae62c1
Shundrik <102665832+Shundrik@users.noreply.github.com>
2022-08-28 13:40:40 +0300

    hw-6
    8 task

--- js/task-08.js
+++ js/task-08.js
@@ -1,23 +1,37 @@
 const form = document.querySelector(".login-form");
 
 form.addEventListener("submit", onFormSubmit);
-const array = [];
 
 function onFormSubmit(event) {
   event.preventDefault();
 
-  const emailEl = event.currentTarget.email.value;
-  const passwordEl = event.currentTarget.password.value;
+  // const emailEl = event.currentTarget.email.value;
+  // const passwordEl = event.currentTarget.password.value;
 
-  if (emailEl === "" || passwordEl === "") {
-    
+  const emailEl = document.querySelector("input[name=email]");
+  const passwordEl = document.querySelector("input[name=password]");
+
+  if (emailEl.value === "" || passwordEl.value === "") {
     alert("applay the form");
+  } else {
+    console.log({ email: emailEl.value, password: passwordEl.value });
+    form.reset();
   }
-    else {
-      console.log( {email: emailEl, password:passwordEl});
-      form.reset();
+  return;
+}
 
-    }
-    return;
-  }
+// const form = document.querySelector('.login-form');
 
+// form.addEventListener('submit', function (event) {
+//     event.preventDefault();
+//     const {
+//         elements: { email, password }
+//     } = event.currentTarget;
+//       if(email.value.length === 0 || password.value.length === 0) {
+//         alert('все поля должны быть заполнены!');
+//       } else {
+//           console.log({ email: email.value, password: password.value });
+//     form.reset();
+//     }
+
+// });
--- js/task-10.js
+++ js/task-10.js
@@ -9,20 +9,8 @@ const refs = {
   boxes: document.querySelector("#boxes"),
 };
 
-// refs.input.addEventListener("input",onInput);
 refs.create.addEventListener("click", createColorBoxes);
 refs.destroy.addEventListener("click", destroyBoxes);
-
-// let number = 0 ;
-
-// function onInput(event) {
-//     +event.currentTarget.value;
-// if(+event.currentTarget.value >= 1 ){
-//   let number = +event.currentTarget.value;
-//     console.log(number);
-//     return number;
-//   }
-// }
 
 function createColorBoxes(amount) {
   let inputValue = +refs.input.value;
@@ -33,16 +21,15 @@ function createColorBoxes(amount) {
   if (inputValue >= 1) {
     for (let index = 0; index < inputValue; index++) {
       console.log(index);
-      //  let width = 100;
+
       width += 30;
-      //  let height = 100;
+
       height += 30;
       const divColorEl = `<div style="width:${width}px;
     height:${height + 10}px;background-color: ${getRandomHexColor()};
     text-align: center; text-align: center;position: absolute;
     display: flex;
     align-items: flex-end;">${getRandomHexColor()}</div>`;
-      // console.log(divColorEl);
       refs.boxes.insertAdjacentHTML("afterbegin", divColorEl);
       console.log(divColorEl);
       divColorEl;
@@ -51,7 +38,6 @@ function createColorBoxes(amount) {
 }
 
 function destroyBoxes(event) {
-  // console.log(event);
   refs.boxes.innerHTML = " ";
   refs.input.value = "";
   console.clear();
